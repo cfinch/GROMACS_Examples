3401ee29e9705b0c5944ff50ecde77325600ae90
Craig Finch <cfinch@ieee.org>
2013-01-04 18:14:41 -0500

    Updated MDP files for NPT constrained and NPT unconstrained
    Changed options to match official tutorial more closely
    
    Changes to run/run_GROMACS_MD.bat:
    - Now allowing all GROMACS output to go to standard out, since redirection wasn't working
    - Only MPI/Torque/Moab output goes to .out file
    - Changed names of output files to clarify which output has constrained atoms

--- speptide/run/run_GROMACS_MD.bat
+++ speptide/run/run_GROMACS_MD.bat
@@ -1,5 +1,4 @@
-#!/bin/bash
-#PBS -l nodes=2:ppn=8,pmem=2000M,walltime=01:00:00
+#PBS -l nodes=2:ppn=12,pmem=2000M,walltime=04:00:00
 #PBS -N GROMACS
 #PBS -A bgoldiez
 #PBS -q batch128
@@ -25,37 +24,38 @@ export NP=`wc -l $PBS_NODEFILE | cut -d' ' -f1`
 echo "Stating Gromacs MD run" >> GROMACS-$PBS_JOBID.out
 
 # ----- Initial energy minimization -----
-echo " ***** Energy Minimization ****** " >> GROMACS-$PBS_JOBID.out
-mpirun -np $NP -machinefile $PBS_NODEFILE mdrun -pd -v -deffnm em >> GROMACS-$PBS_JOBID.out
+echo " ***** Energy Minimization ****** "
+mpirun -np $NP -machinefile $PBS_NODEFILE mdrun -pd -v -deffnm em 
 echo " " >> GROMACS-$PBS_JOBID.out
 
 # ------ NVT with fixed atoms -----
-echo " ***** NVT MD, Fixed Atoms ****** " >> GROMACS-$PBS_JOBID.out
-grompp -maxwarn 3 -f NVT_fixed_atoms.mdp -c em.gro -p topol.top -o nvt.tpr >> GROMACS-$PBS_JOBID.out
-echo " " >> GROMACS-$PBS_JOBID.out
+echo " ***** NVT MD, Fixed Atoms ****** "
+grompp -maxwarn 3 -f NVT_fixed_atoms.mdp -c em.gro -p topol.top -o nvt_constrained.tpr
+# >> GROMACS-$PBS_JOBID.out
+echo " "
 
-mpirun -np $NP -machinefile $PBS_NODEFILE mdrun -pd -deffnm nvt >> GROMACS-$PBS_JOBID.out
-echo " " >> GROMACS-$PBS_JOBID.out
+mpirun -np $NP -machinefile $PBS_NODEFILE mdrun -pd -deffnm nvt_constrained
+echo " "
 
 # ----- NPT with fixed atoms -----
-echo " ***** NPT MD, Fixed Atoms ****** " >> GROMACS-$PBS_JOBID.out
-grompp -maxwarn 3 -f NPT_fixed_atoms.mdp -c em.gro -p topol.top -o npt.tpr >> GROMACS-$PBS_JOBID.out
-echo " " >> GROMACS-$PBS_JOBID.out
+echo " ***** NPT MD, Fixed Atoms ****** "
+grompp -maxwarn 3 -f NPT_fixed_atoms.mdp -c em.gro -p topol.top -o npt_constrained.tpr
+echo " "
 
-mpirun -np $NP -machinefile $PBS_NODEFILE mdrun -pd -deffnm npt >> GROMACS-$PBS_JOBID.out
-echo " " >> GROMACS-$PBS_JOBID.out
+mpirun -np $NP -machinefile $PBS_NODEFILE mdrun -pd -deffnm npt_constrained
+echo " "
 
 # ----- NPT with no constraints -----
-echo " ***** NPT MD, Unconstrained ****** " >> GROMACS-$PBS_JOBID.out
-grompp -maxwarn 3 -f NPT.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_1.tpr >> GROMACS-$PBS_JOBID.out
-echo " " >> GROMACS-$PBS_JOBID.out
+echo " ***** NPT MD, Unconstrained ****** "
+grompp -maxwarn 3 -f NPT.mdp -c npt_constrained.gro -t npt_constrained.cpt -p topol.top -o md_0_1.tpr
+echo " "
 
-mpirun -np $NP -machinefile $PBS_NODEFILE mdrun -pd -v -deffnm md_0_1 >> GROMACS-$PBS_JOBID.out
-echo " " >> GROMACS-$PBS_JOBID.out
+mpirun -np $NP -machinefile $PBS_NODEFILE mdrun -pd -v -deffnm md_0_1
+echo " "
 
-echo "Completed Gromacs MD Run" >> GROMACS-$PBS_JOBID.out
-echo " " >> GROMACS-$PBS_JOBID.out
 ##EDIT ABOVE ***********************************************************
+echo "Completed Gromacs MD Run"
+echo " "
 
 ##DO NOT EDIT BELOW ====================================================
 cat $PBS_NODEFILE >> GROMACS-$PBS_JOBID.out
